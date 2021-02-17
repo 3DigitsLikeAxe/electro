0083d3a54e46e21be182be6a99aed5f536786ee7
Paulin <candywednesday@gmail.com>
2021-02-17 17:15:20 +0300

    auth and welcome layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,25 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.scripts([
+	'resources/js/jquery.min.js',
+	'resources/js/bootstrap.min.js',
+	'resources/js/slick.min.js',
+	'resources/js/nouislider.min.js',
+	'resources/js/jquery.zoom.min.js',
+	'resources/js/main.js'
+	], 'public/js/all.js');
+mix.styles([
+	'resources/css/bootstrap.min.css',
+	'resources/css/slick.css',
+	'resources/css/slick-theme.css',
+	'resources/css/nouislider.min.css',
+	'resources/css/style.css'
+	], 'public/css/all.css');
+
+
